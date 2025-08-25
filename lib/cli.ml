520d8b8a2c17d30fb43acb0efd270d4ae5b47dc9
DrinoSan <s.becirbegovic@gmail.com>
2025-08-25 22:34:27 +0200

    Continued work on cli.ml and added unix to libraries

--- lib/cli.ml
+++ lib/cli.ml
@@ -30,3 +30,37 @@ let list_cmd =
   in
   ( Term.(const list_tasks_cmd $ tasks_file),
     Cmd.info "list" ~doc:"List all tasks" )
+
+let remind_cmd tasks_file =
+  let tasks = load_tasks tasks_file in
+  let now = Unix.gettimeofday () in
+  let reminders =
+    List.filter
+      (fun task ->
+        let reminder_time =
+          task.due_date -. float_of_int task.reminder_offset
+        in
+        task.status = "pending" && now >= reminder_time && now <= task.due_date)
+      tasks
+  in
+  List.iter
+    (fun task ->
+      print_endline ("Reminder: " ^ string_of_task task);
+      match task.reminder_type with
+      | Email -> ()
+      | Sms -> ()
+      | Sys_notification ->
+          ignore
+            (Sys.command
+               ("notify-send 'Task Reminder' '" ^ string_of_task task ^ "'")))
+    reminders
+
+let add_cmd = 
+  let id = Arg.(required & pos 0 (some int) None & info [] ~docv:"ID") in
+(*   let id = Arg.required & Arg.pos 0 (Arg.some Arg.int) None & Arg.info [] ~docv:"ID"   <-- Could also be written like this but above line uses open scoping*)
+  let title = Arg.(required & pos 1 (some string) None & info [] ~docv:"TITLE") in
+  let description = Arg.(required & pos 2 (some string) None & info [] ~docv:"DESCRIPTION") in
+
+
+
+
